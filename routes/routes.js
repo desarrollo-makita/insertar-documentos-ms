92f9d93a929942e51f0aa57d93cf3c2baaccdcc9
jherrera <jherrera@makita.cl>
2024-07-22 16:12:29 -0400

    cambio de nombre en archivo controlador

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { insertarDocumentos } = require('../controllers/insertarDocumentosControllers');
+const { insertarDocumentos } = require('../controllers/insertardocumentosControllers');
 
 router.post('/insertar-documentos', insertarDocumentos);
 
