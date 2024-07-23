998bd3bc0dcb0a197606d513b637cff7ed8e27db
jherrera <jherrera@makita.cl>
2024-07-23 08:57:51 -0400

    modificacion de respuesta de salida

--- controllers/insertardocumentosControllers.js
+++ controllers/insertardocumentosControllers.js
@@ -15,14 +15,20 @@ async function insertarDocumentos(req , res) {
     let data = req.body
     logger.info(`Iniciamos la funcion insertarDocumentos ${JSON.stringify(data)}`);
     let result;
-    let responseData = [];
+  
+    let responseDataList;
+    let response = [];
     
     try {
         // Conectarse a la base de datos 'telecontrol'
         await connectToDatabase('Telecontrol');
+
+        responseDataList =  prepareData(data);
       
+
+        console.log("****VAMOS A ITERAR SOBRE ESTE ARREGLO DE DATOS" , responseDataList);
         // Armamos data que vamos a mandar al procedimiento almacenado
-        for (const documento of data) {
+        for (const documento of responseDataList) {
            
             const request = new sql.Request(); // Nueva instancia de request en caditeracióna 
            
@@ -34,40 +40,48 @@ async function insertarDocumentos(req , res) {
                 fecha: Fecha,
                 usuario: Usuario,
                 descripcion : Descripcion,
-                ordenID  : ID_OS
+                os  : ID_OS
             
             } = documento;
            
             // Ejecutar el procedimiento almacenado con los parámetros
              result = await request
-                .input('ID_Pedido', sql.VarChar(40), ID_Pedido)
+                .input('ID_Pedido', sql.VarChar(40), ID_Pedido !== undefined ? ID_Pedido.toString() : null)
                 .input('Empresa', sql.VarChar(40), "Makita")
                 .input('Tabla', sql.VarChar(40), Tabla)
                 .input('Subtabla', sql.VarChar(40), Subtabla)
-                .input('Archivo', sql.VarChar(200), Archivo.trim())
+                .input('Archivo', sql.VarChar(200), Archivo)
                 .input('Fecha', sql.VarChar, formatDate(Fecha))
                 .input('Usuario', sql.VarChar(40), Usuario)
                 .input('Descripcion', sql.VarChar(40), Descripcion)
-                input('ID_OS', sql.VarChar(20), ID_OS)
-            2  .output('Insertado', sql.Int)
+                .input('ID_OS', sql.VarChar(20),  ID_OS !== undefined ? ID_OS.toString() : null)
+                .output('Insertado', sql.Int)
                 .output('ResultadoID', sql.VarChar)
       
                 .execute('insertaDocumentosSP');
                 
                 result.data = documento;
-                responseData.push(result);
+                let resultSP = {
+                    data : documento,
+                    output : result.output
+                }
+                
+                
+                response.push(resultSP);
                
                
         }
         
-        await closeDatabaseConnection();
-        logger.info(`Fin de la funcion insertarDocumentos ${JSON.stringify(responseData)}`);
-        res.status(200).json(responseData);
+        logger.info(`Fin de la funcion insertarDocumentos ${JSON.stringify(response)}`);
+        res.status(200).json(response);
     
     } catch (error) {
         // Manejamos cualquier error ocurrido durante el proceso
         logger.error(`Error en insertarDocumentos: ${error.message}`);
         res.status(500).json({ error: `Error en el servidor [insertar-documentos-ms] :  ${error.message}`  });
+    }
+    finally{
+        await closeDatabaseConnection();
     }
 }
 
@@ -77,15 +91,38 @@ async function insertarDocumentos(req , res) {
  * @returns 
  */
 function formatDate(date) {
-    console.log("fechaFormated : " , date); 
     if(date != null){
         const fechaMoment = moment(date, "DD-MM-YYYY");
         const fechaFormateada = fechaMoment.format("YYYY-MM-DD");
-        console.log("fechaFormated : " , fechaFormateada); 
         return fechaFormateada;
     }
 }
 
+
+function prepareData(data) {
+    let request = [];
+    let req = {};
+    for(element of data){
+        for(archivo of element.arregloLink){
+            req = {
+                tabla: "DOCUMENTO",
+                subtabla : "NOTA DE VTA INTERNA",
+                archivo :archivo.link,
+                fecha: archivo.data,
+                usuario : element.cnpj ,
+                descripcion: `Documento de la orden ${element.os}`,
+                idPedido : element.idPedido,
+                os: element.os
+            }
+
+            request.push(req);
+
+        }
+    }
+
+    return request;
+  
+}
 module.exports = {
     insertarDocumentos
 };
